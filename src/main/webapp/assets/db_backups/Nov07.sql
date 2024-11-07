67219358bf1cc7c8a84b77c2f20c17cb374a2b65
sumindadissanayaka <suminda@graycorp.com.au>
2024-11-07 11:37:59 -0500

    db dump added

--- src/main/webapp/assets/db_backups/Nov07.sql
+++ src/main/webapp/assets/db_backups/Nov07.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hms`;
 -- MySQL dump 10.13  Distrib 8.0.36, for macos14 (x86_64)
 --
 -- Host: 127.0.0.1    Database: hms
@@ -14,6 +16,33 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Department`
+--
+
+DROP TABLE IF EXISTS `Department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Department` (
+  `departmentId` int NOT NULL AUTO_INCREMENT,
+  `departmentName` varchar(100) NOT NULL,
+  `headOfDepartmentId` int DEFAULT NULL,
+  PRIMARY KEY (`departmentId`),
+  KEY `fk_headOfDepartment` (`headOfDepartmentId`),
+  CONSTRAINT `fk_headOfDepartment` FOREIGN KEY (`headOfDepartmentId`) REFERENCES `User` (`userId`) ON DELETE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Department`
+--
+
+LOCK TABLES `Department` WRITE;
+/*!40000 ALTER TABLE `Department` DISABLE KEYS */;
+INSERT INTO `Department` VALUES (1,'Surgical',NULL),(2,'ICU',NULL),(3,'ETU',NULL),(4,'OPD',NULL),(5,'Lab',NULL);
+/*!40000 ALTER TABLE `Department` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `User`
@@ -50,6 +79,30 @@ LOCK TABLES `User` WRITE;
 INSERT INTO `User` VALUES (1,'Admin','User','admin@example.com','admin',4,'123-456-7890','123 Admin St',NULL);
 /*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `UserRole`
+--
+
+DROP TABLE IF EXISTS `UserRole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `UserRole` (
+  `roleId` int NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(50) NOT NULL,
+  PRIMARY KEY (`roleId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `UserRole`
+--
+
+LOCK TABLES `UserRole` WRITE;
+/*!40000 ALTER TABLE `UserRole` DISABLE KEYS */;
+INSERT INTO `UserRole` VALUES (1,'Doctor'),(2,'Nurse'),(3,'Technician'),(4,'Admin');
+/*!40000 ALTER TABLE `UserRole` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -60,4 +113,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-07 11:24:55
+-- Dump completed on 2024-11-07 11:37:24
